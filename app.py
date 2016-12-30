ca7176ee44013a53e10f20f4f5f63714d898ae66
himanoa <matsunoappy@gmail.com>
2016-12-31 00:51:34 +0900

    poepoe

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ app = Flask("ChatServer")
 
 @app.after_request
 def after_request(response):
+    response = jsonify({"status": "ok"})
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    response.status_code = 200
     return response
 
 @app.route("/", methods=["POST"])
@@ -19,9 +21,6 @@ def chat():
     data = request.data.decode('utf-8')
     print(data)
     firebase.post('/boards/chat', json.loads(data))
-    response = jsonify({"status": "ok"})
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
     response.status_code = 201
     return response
 
