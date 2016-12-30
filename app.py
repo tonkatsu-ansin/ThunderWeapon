17cd50e841e585f2020faefde7b08f326ab89c91
himanoa <matsunoappy@gmail.com>
2016-12-30 23:54:56 +0900

    add afterrequest

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import os
 firebase = firebase.FirebaseApplication(os.getenv("FIREBASE_URL"), None)
 app = Flask("ChatServer")
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 @app.route("/", methods=["POST"])
 def chat():
