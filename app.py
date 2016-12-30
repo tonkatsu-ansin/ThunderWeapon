154e7b6722f7ef1e3d2394438ee71bbb85210929
himanoa <matsunoappy@gmail.com>
2016-12-31 01:07:33 +0900

    implemets flask-cors

--- app.py
+++ app.py
@@ -2,26 +2,11 @@ from flask import Flask, request, jsonify
 from firebase import firebase
 import json
 import os
-
+from flask.ext.cors import CORS
 
 firebase = firebase.FirebaseApplication(os.getenv("FIREBASE_URL"), None)
 app = Flask("ChatServer")
-
-@app.after_request
-def after_request(response):
-    response = jsonify({"status": "ok"})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    response.status_code = 200
-    return response
-
-@app.route("/", methods=["OPTIONS"])
-def preflight():
-    response = jsonify({"status": "ok"})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'OPTIONS, GET,PUT,POST,DELETE')
-    response.status_code = 201
-    return response
+cors = CORS(app)
 
 @app.route("/", methods=["POST"])
 def chat():
