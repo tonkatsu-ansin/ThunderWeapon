a3142c432a5c5a8a49173dcc2acc2c2e60ff8815
himanoa <matsunoappy@gmail.com>
2016-12-31 00:58:23 +0900

    tanomu

--- app.py
+++ app.py
@@ -15,14 +15,31 @@ def after_request(response):
     response.status_code = 200
     return response
 
+@app.route("/", methods=["OPTIONS"])
+def preflight():
+    response = jsonify({"status": "ok"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'OPTIONS, GET,PUT,POST,DELETE')
+    response.status_code = 201
+    return response
+
 @app.route("/", methods=["POST"])
 def chat():
     data = request.data.decode('utf-8')
-    firebase.post('/boards/chat', json.loads(data))
+    try:
+        firebase.post('/boards/chat', json.loads(data))
 
-    response = jsonify({"status": "ok"})
-    response.status_code = 201
-    return response
+        response = jsonify({"status": "ok"})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Methods', 'OPTIONS, GET,PUT,POST,DELETE')
+        response.status_code = 201
+        return response
+    except:
+        response = jsonify({"status": "fail"})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Methods', 'OPTIONS, GET,PUT,POST,DELETE')
+        response.status_code = 400
+        return response
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8403)
