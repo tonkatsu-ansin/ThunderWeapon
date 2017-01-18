bf3d832d5a472473caf8f892872e95980730b98b
himanoa <matsunoappy@gmail.com>
2017-01-19 00:14:42 +0900

    add root endpoint test

---
+++ tests/test_root.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from app import app
+import json
+
+class TestChat:
+
+    @classmethod
+    def setup_class(cls):
+        cls.client = app.test_client()
+
+    def test_valid(self):
+        data = json.dumps({
+            "color": "#0000",
+            "user": "testUser",
+            "text": "これはテストなんだぞ！"
+        })
+        response = self.client.post("/", data=data)
+        assert response.status_code == 201
+
+    def test_invalid_empty_color(self):
+        data = json.dumps({
+            "user": "testUser",
+            "text": "これはテストなんだぞ！"
+        })
+        response = self.client.post("/", data=data)
+        assert response.status_code == 400
+
+    def test_invalid_empty_user(self):
+        data = json.dumps({
+            "color": "#0000",
+            "text": "これはテストなんだぞ！"
+        })
+        response = self.client.post("/", data=data)
+        assert response.status_code == 400
+
+    def test_invalid_empty_text(self):
+        data = json.dumps({
+            "color": "#0000",
+            "user": "testUser",
+        })
+        response = self.client.post("/", data=data)
+        assert response.status_code == 400
+
+    def test_invalid_empty_text_string(self):
+        data = json.dumps({
+            "color": "#0000",
+            "user": "testUser",
+            "text": ""
+        })
+        response = self.client.post("/", data=data)
+        assert response.status_code == 400
+
+    def test_invalid_empty_text_user(self):
+        data = json.dumps({
+            "color": "#0000",
+            "user": "",
+            "text": "これはテストなんだぞ"
+        })
+        response = self.client.post("/", data=data)
+        assert response.status_code == 400
+
+    def test_invalid_empty_text_color(self):
+        data = json.dumps({
+            "color": "",
+            "user": "testUser",
+            "text": "これはテストなんだぞ"
+        })
+        response = self.client.post("/", data=data)
+        assert response.status_code == 400
+
+    def test_invalid_color_code(self):
+        data = json.dumps({
+            "color": "ningensei",
+            "user": "testUser",
+            "text": "これはテストなんだぞ"
+        })
+        response = self.client.post("/", data=data)
+        assert response.status_code == 400
