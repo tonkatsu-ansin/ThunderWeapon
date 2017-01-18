e8218d7c3b0a5794176b84e2f4cc4cb27d0134a6
himanoa <matsunoappy@gmail.com>
2017-01-19 00:49:38 +0900

    fix lint

--- manage.py
+++ manage.py
@@ -3,9 +3,9 @@
 from flask_script import Manager
 from os.path import join, dirname
 from dotenv import load_dotenv
+from app import app
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-from app import app
 manager = Manager(app)
 
 if __name__ == "__main__":
