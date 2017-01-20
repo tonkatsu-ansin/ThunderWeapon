8663f3e5f17bf191678f6e614f3abc979aacd207
naari3 <qazaqaza98@gmail.com>
2017-01-20 20:51:06 +0900

    fix test case

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -4,7 +4,18 @@ from io import BytesIO
 from app import app
 from faker import Factory
 from json import loads
+from functools import wraps
 
+def conentlength(size):
+    def _contentlength(f):
+        tmp = app.config["MAX_CONTENT_LENGTH"]
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            app.config["MAX_CONTENT_LENGTH"] = size
+            f(*args, **kwargs)
+            app.config["MAX_CONTENT_LENGTH"] = tmp
+        return wrapper
+    return _contentlength
 
 class TestUpload:
 
@@ -19,8 +30,9 @@ class TestUpload:
         assert response.status_code == 201
         assert loads(response.data.decode('utf-8')).get('status') == 'ok'
 
+    @conentlength(16 * 1024)
     def test_invalid_over_filesize(self):
         response = self.client.post('/upload', data={'file': (BytesIO(
-            b'a' * 1024 * 1024  * 16), 'fuga.txt')}) # 16MB file upload
+            b'a' * 1024 * 16), 'fuga.txt')}) # 16MB file upload
         assert response.status_code == 413
         assert loads(response.data.decode('utf-8')).get('status') == 'fail'
