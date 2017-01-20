89b5738fcd182fa5d7e92adfb7eb6a53f891191a
himanoa <matsunoappy@gmail.com>
2017-01-20 12:55:50 +0900

    WIP fix test case

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -21,6 +21,6 @@ class TestUpload:
 
     def test_invalid_over_filesize(self):
         response = self.client.post('/upload', data={'file': (BytesIO(
-            b'a' * 1024 * 1024 * 1024 * 16), 'fuga.txt')})
+            b'a' * 1024 * 1024  * 16), 'fuga.txt')}) # 16MB file upload
         assert response.status_code == 413
         assert loads(response.data.decode('utf-8')).get('status') == 'fail'
